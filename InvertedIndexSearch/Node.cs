e5f73485dcb36c50b2f6ae7be6cd83c9b5f72203
Maxim <m.vasyuk@pressfoto.ru>
2022-03-07 19:45:55 +0600

    Added Node

---
+++ InvertedIndexSearch/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace InvertedIndexSearch;
+
+public class Node
+{
+    public Node leftNode { get; set; }
+    public Node rightNode { get; set; }
+    public string data { get; set; }
+
+    public void ViewNode()
+    {
+        Console.WriteLine(data);
+        Console.WriteLine("L:{0}\tR:{1}", leftNode.data, rightNode.data);
+    }
+}
